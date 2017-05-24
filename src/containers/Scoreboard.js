e93e133f7a80745d9cc88deb231b263bb45b34a3
Bessem <bessem.mouelhi@outlook.fr>
2017-05-24 20:02:47 +0200

    add actions, actiontypes and reducers for players

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,97 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Player from '../components/Player';
 import Header from '../components/Header';
 import AddPlayerForm from '../components/AddPlayerForm';
 
-const INITIAL_STATE = {
-  players: [
-    {
-      id: 1,
-      name: "John Hopkins",
-      score: 54
-    },
-    {
-      id: 2,
-      name: "Bruce Smith",
-      score: 59
-    },
-    {
-      id: 3,
-      name: "Linda Afflek",
-      score: 65
-    },
-  ],
-}
+export default class Scoreboard extends Component {
 
-/*function Header(props) {
-  return (
-    <div className="panel-heading">
-      <div className="row">
-        <Stats players={props.players} />
-        <div className="title col-sm-6 text-center text-uppercase">{props.title}</div>
-        <Stopwatch />
-      </div>
-    </div>
-  )
-}
+  state = {
+    players: [
+      {
+        id: 1,
+        name: "John Hopkins",
+        score: 54
+      },
+      {
+        id: 2,
+        name: "Bruce Smith",
+        score: 59
+      },
+      {
+        id: 3,
+        name: "Linda Afflek",
+        score: 65
+      },
+    ],
+  };
 
-Header.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  players: React.PropTypes.array.isRequired
-}*/
-
-/*function Player(props) {
-  return (
-    <li className="list-group-item">
-      <div className="row">
-        <div className="title col-xs-8">
-          <a className="btn btn-danger btnRemove" onClick={props.onRemove} >
-            <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
-          </a>
-          {"  "}{props.name}
-        </div>
-        <div className="col-xs-4">
-          <Counter score={props.score} onChange={props.onScoreChange} />
-        </div>
-      </div>
-    </li>
-  )
-}
-
-Player.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.number.isRequired,
-  onScoreChange: React.PropTypes.func.isRequired,
-  onRemove: React.PropTypes.func.isRequired,
-}*/
-
-const Scoreboard = React.createClass({
-  propTypes: {
-    title: React.PropTypes.string.isRequired,
-    initialPlayers: React.PropTypes.arrayOf(React.PropTypes.shape({
-      id: React.PropTypes.number.isRequired,
-      name: React.PropTypes.string.isRequired,
-      score: React.PropTypes.number.isRequired
-    })).isRequired,
-  },
-
-  getDefaultProps: function () {
+  getDefaultProps() {
     return {
       title: 'Scoreboard'
     };
-  },
+  };
 
-  getInitialState: function () {
-    return INITIAL_STATE;
-  },
-
-  onScoreChange: function (index, delta) {
+  onScoreChange = (index, delta) => {
     console.log('onScoreChange', index, delta);
     this.state.players[index].score += delta;
     this.setState(this.state);
-  },
+  };
 
-  onPlayerAdd: function (name) {
+  onPlayerAdd = (name) => {
     console.log('onPlayerAdd', name, this.state.players.length + 1);
     this.state.players.push({
       name: name,
@@ -99,15 +45,15 @@ const Scoreboard = React.createClass({
       id: this.state.players.length + 1
     })
     this.setState(this.state)
-  },
+  };
 
-  onRemovePlayer: function (index) {
+  onRemovePlayer = (index) => {
     console.log('onRemovePlayer', index);
     this.state.players.splice(index, 1);
     this.setState(this.state);
-  },
+  };
 
-  render: function () {
+  render() {
     return (
       <div className="application">
         <div className="panel panel-primary">
@@ -126,7 +72,5 @@ const Scoreboard = React.createClass({
 
       </div>
     )
-  }
-})
-
-export default Scoreboard;
+  };
+}
